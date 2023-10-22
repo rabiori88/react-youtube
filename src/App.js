2c64788efe2799f18bde8b953ee0c291668cd439
Rabiori <ctangtangbbang@gmail.com>
2023-10-22 12:10:11 +0900

    get data 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./components/SearchHeader";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return (
-   <>
-    <SearchHeader />
-    <Outlet />
-   </>
+  const queryClient = new QueryClient();
 
+  return (
+    <>
+      <SearchHeader />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </>
   );
 }
 
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,11 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from  'react'
 import { useParams } from 'react-router-dom'
+import VideoCard from '../components/VideoCard';
+import axios from 'axios';
+
 
 export default function Videos() {
     const {keyword} = useParams();
+    const {
+        isLoading,
+        error,
+        data: videos,
+    } = useQuery(
+        ['videos', keyword] , async() => {
+            console.log(keyword);
+            return axios.get(`/data/${keyword? 'keyword' : 'trend'}.json`)            
+        .then((res)=> res.data.items)
+        
+        }
+    );
+
     return ( 
         <div>
-            <p>Viedos {keyword? `🔎${keyword}` : `🚀`} </p>           
+            <p>Viedos {keyword? `🔎${keyword}` : `🚀`} </p>   
+            {isLoading && <p>Loading...</p> }
+            {error && <p>Something is wrong</p>}
+            {videos && <ul>
+                {
+                    videos.map(video => <VideoCard key={video.id} video={video}/>) 
+                }</ul>}
         </div>
     )
 }
