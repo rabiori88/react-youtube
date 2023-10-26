b18c4a9cc8513a4884bbaac983e8131b9da5193b
Rabiori <ctangtangbbang@gmail.com>
2023-10-26 23:06:05 +0900

    videoDetail 프레임 까지 구현

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,9 +1,38 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from  'react'
+import { useLocation } from 'react-router-dom'
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideoDetail() {
+    const {
+        state: {video}
+    } = useLocation();
+
+
+    const{title , channelId , channelTitle, description} = video.snippet;
+
     return ( 
-        <div>
-            <p>Video Dataile</p>
-        </div>
+        <section>
+            <article>
+                <iframe
+                id='player'
+                type='text/html'
+                width='100%'
+                height='640'
+                src={`http://www.youtube.com/embed/${video.id}`}
+                
+                />
+                <div>
+                    <h2>{title}</h2>
+                    <ChannelInfo id={channelId}  name={channelTitle}/>
+                    <pre>{description}</pre>
+                </div>
+            </article>
+            <section>
+                <RelatedVideos id={video.id} />
+            </section>
+
+        </section>
     )
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -21,7 +21,7 @@ export default function Videos() {
             <p>Viedos {keyword? `🔎${keyword}` : `🚀`} </p>   
             {isLoading && <p>Loading...</p> }
             {error && <p>Something is wrong</p>}
-            {videos && <ul>
+            {videos && <ul className='grid grid-cols-1 sm:grid-cols-2 lg:graid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
                 {
                     videos.map(video => <VideoCard key={video.id} video={video}/>) 
                 }</ul>}
